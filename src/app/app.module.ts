ca907522162a19369bb9bd57c8354ef393df9bab
AleCosta <alecostabr@gmail.com>
2024-02-12 18:59:54 -0300

    refatorado routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,7 @@ import { MatListModule } from '@angular/material/list';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosModule } from './cursos/cursos.module';
 
 
@@ -21,10 +17,8 @@ import { CursosModule } from './cursos/cursos.module';
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent//,
-    //CursosComponent,
-    //CursoDetalheComponent,
-    //CursoNaoEncontradoComponent
+    LoginComponent
+
   ],
   imports: [
     BrowserModule,
--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { RouterModule } from '@angular/router';
+//import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
@@ -9,6 +9,7 @@ import { CursosComponent } from "./cursos.component";
 import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
 import { CursoNaoEncontradoComponent } from "./curso-nao-encontrado/curso-nao-encontrado.component";
 import { CursosService } from "./cursos.service";
+import { CursosRoutingModule } from "./cursos.routing.module";
 
 @NgModule({
     imports:[
@@ -16,7 +17,7 @@ import { CursosService } from "./cursos.service";
         MatToolbarModule,
         MatButtonModule,
         MatListModule,
-        RouterModule
+        CursosRoutingModule
     ],
     exports:[],
     declarations:[
